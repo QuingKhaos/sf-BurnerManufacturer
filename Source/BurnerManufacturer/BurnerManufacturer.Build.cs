c444f1d9ca75ba0f563080e6792ce14e89d8d4eb
Em Karisch <emii.khaos@gmail.com>
2026-09-05 11:10:34 +0200

    chore: init mod

--- Source/BurnerManufacturer/BurnerManufacturer.Build.cs
+++ Source/BurnerManufacturer/BurnerManufacturer.Build.cs
@@ -2,9 +2,9 @@ using UnrealBuildTool;
 using System.IO;
 using System;
 
-public class ###NAME### : ModuleRules
+public class BurnerManufacturer : ModuleRules
 {
-	public ###NAME###(ReadOnlyTargetRules Target) : base(Target)
+	public BurnerManufacturer(ReadOnlyTargetRules Target) : base(Target)
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 		CppStandard = CppStandardVersion.Cpp20;
